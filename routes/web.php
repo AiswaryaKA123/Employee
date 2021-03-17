c640d67d42bc2e338c1b3bb291e3a7509cc03969
AiswaryaKA123 <aiswaryakasokan@gmail>
2021-03-17 15:07:45 +0530

    changed

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-route::get('/',function(){
-    return view('search');
+
+Route::get('/searchemp',function(){
+    return view('searchemp);
 
 });
