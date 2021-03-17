0cbc29690a2646fb0d746d6400b6dcce631c41e2
AiswaryaKA123 <aiswaryakasokan@gmail>
2021-03-17 15:32:47 +0530

    navigation

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('addemp');
 });
 
 Route::get('/searchemp',function(){
-    return view('searchemp);
+    return view('searchemp');
 
 });
